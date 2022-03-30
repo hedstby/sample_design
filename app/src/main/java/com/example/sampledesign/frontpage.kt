f63e40150496bc907827d813edd69179021f7ddb
呂艾庭 <hedstby@lvaitingdeMacBook-Pro.local>
2022-03-31 00:41:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sampledesign/frontpage.kt
@@ -0,0 +1,11 @@
+package com.example.sampledesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class frontpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frontpage)
+    }
+}
